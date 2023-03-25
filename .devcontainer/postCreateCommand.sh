aa07b403ec1b441dc615107b07b5be60e5d7bc72
Lars Kruse <lakruzz@inc-inc.dk>
2023-03-25 07:53:26 +0000

    added pscale CLI and mysql CLI to devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+sudo chown vscode:vscode /home/vscode/.m2 /home/vscode/.gradle
+
+sudo apt-get update
+sudo apt-get install -y mysql-client
+
+curl -LO https://github.com/planetscale/cli/releases/download/v0.131.0/pscale_0.131.0_linux_amd64.deb
+sudo dpkg -i pscale_0.131.0_linux_amd64.deb
+rm pscale_0.131.0_linux_amd64.deb
